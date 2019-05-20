0e0d8109ff6f50876c5a3d11ef87aae61bf9ba6c
litao <195218362@qq.com>
2019-05-20 18:02:01 +0800

    ..

---
+++ mybatiesdemo/src/main/java/com/winter/MybatiesdemoApplication.java
@@ -0,0 +1,17 @@
+package com.winter;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication()
+@MapperScan("com.winter.mapper")
+public class MybatiesdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatiesdemoApplication.class, args);
+    }
+
+}
